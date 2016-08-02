c17e7428fe9ef0fe50c108736aae2cc753ed5e21
Nessa <Dao_ri_zan@hotmail.com>
2016-08-01 22:46:24 -0700

    Added Recast

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+[RequireComponent (typeof (AudioSource))]
+public class Gun : MonoBehaviour {
+
+	public enum GunType {Semi, Burst,Auto};
+	public GunType gunType;
+	public float rpm;
+
+
+	//where the bullet is coming from
+	public Transform spawn;
+
+
+	//System
+	private float secondsBetweenShots;
+	private float nextPossibleShootTime;
+
+	void Start() {
+		secondsBetweenShots = 60 / rpm;
+	}
+	public void Shoot() {
+
+		if (CanShoot ()) {
+
+			Ray ray = new Ray (spawn.position, spawn.forward);
+			RaycastHit hit;
+			float shotDistance = 20;
+			if (Physics.Raycast (ray, out hit, shotDistance)) {
+				//object it hits
+				shotDistance = hit.distance;
+			}
+			//Debug.DrawRay (ray.origin, ray.direction * shotDistance, Color.red, 1);
+
+			nextPossibleShootTime = Time.time + secondsBetweenShots;
+		}
+		GetComponent<AudioSource>().Play ();
+	}
+
+	public void ShootContinuous() {
+		if (gunType == GunType.Auto) {
+			Shoot ();
+		}
+	}
+
+	private bool CanShoot() {
+		bool canShoot = true;
+		if (Time.time < nextPossibleShootTime) {
+			canShoot = false;
+		}
+		return canShoot;
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,34 +16,68 @@ public class PlayerController : MonoBehaviour {
     private Quaternion targetRotation;
 
     //Components
+	public Gun gun;
     private CharacterController controller;
+	private Camera cam;
 
 
 	// Use this for initialization
 	void Start () {
         controller = GetComponent <CharacterController>();
+		cam = Camera.main;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		ControlMouse ();
+		//ControlWASD ();
+		if (Input.GetButtonDown ("Shoot")) {
+			gun.Shoot ();
+		} else if (Input.GetButton ("Shoot")) {
+			gun.ShootContinuous ();
+		}
 
-        if (input != Vector3.zero)
-        {
-            targetRotation = Quaternion.LookRotation(input);
-            transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
-        }
+	}
 
-        Vector3 motion = input;
-        motion += Vector3.up * -8;
-                    //if == 1             and                      1  * by .7 or 1
-        motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
-        motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
+	void ControlMouse() {
+		//bottom left is x = 0 and y = 0
+		//upper right is x = screen.width  and y = screen.height
+		Vector3 mousePos = Input.mousePosition;
+		mousePos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.transform.position.y -transform.position.y));
+		targetRotation = Quaternion.LookRotation (mousePos - new Vector3(transform.position.x, 0, transform.position.z));
+		transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y, 
+																		rotationSpeed * Time.deltaTime);
+
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		Vector3 motion = input;
+		motion += Vector3.up * -8;
+		//if == 1             and                      1  * by .7 or 1
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
+		motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
 
 
-        controller.Move(motion * Time.deltaTime);
+		controller.Move(motion * Time.deltaTime);
+	
+	}
+	void ControlWASD() {
+	
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		if (input != Vector3.zero)
+		{
+			targetRotation = Quaternion.LookRotation(input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y, targetRotation.eulerAngles.y, rotationSpeed * Time.deltaTime);
+		}
+
+		Vector3 motion = input;
+		motion += Vector3.up * -8;
+		//if == 1             and                      1  * by .7 or 1
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1) ? .7f : 1;
+		motion *= (Input.GetButton("Run")) ? runSpeed : walkSpeed;
 
 
+		controller.Move(motion * Time.deltaTime);
 	}
 }
